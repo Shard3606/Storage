15d905a00835fb652dc8fe04f8115a4c420ca5cd
AleCastillooo <167446117+AleCastillooo@users.noreply.github.com>
2024-05-02 10:48:22 -0400

    Update mainmenu.py

--- mainmenu.py
+++ mainmenu.py
@@ -132,61 +132,31 @@ def EditList():
     #Fetch List list and List, print List, make editable
     time.sleep(3)
     Welcome()
-"""
+    
 def ViewList():
     global List1
     global List2
     global List3
     global List4
     global List5
-    global List1
-    global List2
-    global List3
-    global List4
-    global List5
-    global List1Items
-    global List2Items
-    global List3Items
-    global List4Items
-    global List5Items
-    if List1 != ["ListName", "ListText"]:
-        print(List1[1])
-    if List2 != ["ListName", "ListText"]:
-        print(List2[1])
-    if List3 != ["ListName", "ListText"]:
-        print(List3[1])
-    if List4 != ["ListName", "ListText"]:
-        print(List4[1])
-    if List5 != ["ListName", "ListText"]:
-        print(List5[1])
-    ListSelection = input("Please input the name of the List you would like to view, or input 'Back' to go back.")
-    if ListSelection == List1Name:
-        print(List1[2])
-        Back = input("Input anything when you are done viewing.")
-    elif ListSelection == List2Name:
-        print(List2[2])
-        Back = input("Input anything when you are done viewing.")
-    elif ListSelection == List3Name:
-        print(List3[2])    
-        Back = input("Input anything when you are done viewing.")
-    elif ListSelection == List4Name:
-        print(List4[2])
-        Back = input("Input anything when you are done viewing.")
-    elif ListSelection == List5Name:
-        print(List5[2])
-        Back = input("Input anything when you are done viewing.")
-    elif ListSelection == "Back":
-        Continue = ""
-    else:
-        print("Please make sure to write one of the valid List names.")
-        time.sleep(3)
-        ViewList()
-    #Fetch List list and List, print List
-    time.sleep(3)
-    Welcome()
-    time.sleep(3)
-    Welcome()
+    lstuser = input("Enter your username: ")
+    lstpass = input("Enter your password: ")
+    lstfile_path = str(lstuser.lower()) + "," + str(lstpass.lower())
 
+    try:
+        with open(lstfile_path, 'r') as file:
+            # Read the content of the file
+            file_content = file.read()
+
+            # Print the content
+            print("File Content:\n", file_content)
+
+    except FileNotFoundError:
+        print(f"File '{lstfile_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    """
 def DeleteList():
     global List1
     global List2
